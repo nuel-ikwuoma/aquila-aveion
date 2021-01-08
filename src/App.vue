495bdaa66b03c56a790aa14357b9572252ad7503
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2021-01-08 13:58:11 +0100

    initial design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './components/Home'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Courier New', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(to right,#BD304D 50%, #CB3464 50%)
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <header>{{ content.name }}</header>
+    <section>
+      <main>
+      </main>
+      <aside>
+        <a :href="instagram"
+          ><span><i class="fab fa-instagram"></i></span
+        ></a>
+        <a :href="twitter"
+          ><span><i class="fab fa-twitter"></i></span
+        ></a>
+        <a :href="snapchat"
+          ><span><i class="fab fa-snapchat"></i></span
+        ></a>
+        <a :href="email"
+          ><span><i class="fas fa-envelope-square"></i></span
+        ></a>
+      </aside>
+    </section>
+    <nav>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li><img alt="Vue logo" src="../assets/brown_girl.png" /></li>
+        <li>GALLERY</li>
+        <li>CONTACT</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      content: {
+        name: '',
+        twitter: '',
+        instagram: '',
+        snapchat: '',
+        email: '',
+        social_media: [],
+      },
+    };
+  },
+  created() {
+    // fetch the aquilla API
+    fetch('https://hirng-x2021.glitch.me/api')
+      .then((response) => response.json())
+      .then(({name, social_media}) => {
+        this.content.name = name;
+        this.content.twitter = social_media.twitter;
+        this.content.instagram = social_media.instagram;
+        this.content.snapchat = social_media.snapchat;
+        this.content.email = social_media.email;
+      })
+      .catch(console.error);
+  },
+  computed: {
+    twitter: function() {
+      return 'https://www.twitter.com/' + this.content.twitter;
+    },
+    instagram: function() {
+      return 'https://www.instagram.com/' + this.content.instagram;
+    },
+    snapchat: function() {
+      return 'https://www.snapchat.com/' + this.content.snapchat;
+    },
+    email: function() {
+      return 'mailto:' + this.content.email;
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  height: 100px;
+  padding: 10px;
+  font-size: 38px;
+  text-transform: uppercase;
+}
+section {
+  height: 400px;
+  align-items: center;
+  display: flex;
+}
+section main {
+  width: 50%;
+  height: 100%;
+  margin: 0px auto;
+  border: 1px solid inherit;
+  text-align: center;
+  background: rgba(76, 175, 80, 0.6);
+  background-image: url('../assets/girl4.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50% 15%;
+  box-shadow: -1px 1px 22px -8px #3f3a3c;
+  box-sizing: border-box;
+}
+section aside {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 90%;
+  padding: 1em;
+}
+section aside a {
+  padding: 0.4em;
+}
+section aside span {
+  font-weight: 600;
+  font-size: 2.4em;
+  color: white;
+}
+nav ul {
+  list-style-type: none;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+nav li {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  margin: 12px;
+  padding: 12px;
+  cursor: pointer;
+  font-size: 18px;
+}
+nav li img {
+  width: inherit;
+  height: inherit;
+  border-radius: 50%;
+}
+</style>
